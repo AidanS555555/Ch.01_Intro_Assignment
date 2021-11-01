a26f2fb9adb77dac58ed2d184b498deaf1fd69da
swartzaid <swartzaid@urbandaleschools.com>
2021-11-01 09:45:45 -0500

    Number systems commit

---
+++ Chapter 6.py
@@ -0,0 +1,19 @@
+"""""
+It's like the little yellow blocks of plastic from elementary school. You have the thousands place,
+the hundreds place, the tens place, and so on. The first number is always going to be one. The second number
+is going to be the number of your system
+Base ten=0,1,2,3,4,5,6,7,8,9
+"""""
+
+"""""
+Binary system: 0,1
+1 byte=8 bits
+_|_|_|_|_|_|_|
+1|2|4|8|16|32|64|128
+2(0)2(1)2(2)2(3)2(4)2(5)
+all above is reversed
+1011
+"""
+"""
+Octal system: goes by powers of eight. 1,8,64,512, etc.
+Hexadecimal: Base 16 system. Uses letters. 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
